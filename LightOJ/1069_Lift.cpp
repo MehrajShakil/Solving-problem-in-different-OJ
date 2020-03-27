0fc19f9ffaecd788ca716d0cf9cf59ecf74d0e89
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2020-03-27 21:33:57 +0600

    Add files via upload

---
+++ LightOJ/1069_Lift.cpp
@@ -0,0 +1,53 @@
+
+                                            //BISMILLAHIR RAHMANIR RAHEEM
+                                                //ALLAH IS WATCHING ME
+
+//█▀█─█──█──█▀█─█─█
+//█▄█─█──█──█▄█─█▄█
+//█─█─█▄─█▄─█─█─█─█
+                                 //****************************************************
+                                 //**          Date : 14/02/2020(time is over)       **
+                                 //**              Love is not for a day             **
+                                 //**                                                **
+                                 //****************************************************
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ull    unsigned long long
+#define ll    long long
+#define ios    ios::sync_with_stdio(0);cin.tie(0);
+#define ses   "\n"
+
+string digit="0123456789";
+string small="abcdefghijklmnopqrstuvwxyz";
+
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1}; // 8-direction.......
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[]= {1,-1,0,0}; // 4-direction...........
+int dy4[]= {0,0,1,-1};
+
+const int N=1e6+5;
+const int MOD=1e9+7;
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////
+
+//main function is here.........
+int main(){
+    ll tc;
+    cin>>tc;
+    for(ll i=1;i<=tc;i++){
+        ll me,lift;
+        cin>>me>>lift;
+        ll tome=4*abs(me-lift);
+        tome+=(4*me);
+        cout<<"Case "<<i<<": "<<19+tome<<ses;
+    }
+}
+
+
+
+
+//Alhamdulillah
