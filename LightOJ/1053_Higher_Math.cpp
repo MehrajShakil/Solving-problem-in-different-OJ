d3b73e085ff61b47d59d3eea7a2418512f4bc558
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2020-03-27 21:32:48 +0600

    Add files via upload

---
+++ LightOJ/1053_Higher_Math.cpp
@@ -0,0 +1,56 @@
+
+                                            //BISMILLAHIR RAHMANIR RAHEEM
+                                                //ALLAH IS WATCHING ME
+
+//█▀█─█──█──█▀█─█─█
+//█▄█─█──█──█▄█─█▄█
+//█─█─█▄─█▄─█─█─█─█
+                                 //****************************************************
+                                 //**          Date : 14/02/2020(time is over)       **
+                                 //**              Love is not for a day             **
+                                 //**                                                **
+                                 //****************************************************
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ull    unsigned long long
+#define ll    long long
+#define ios    ios::sync_with_stdio(0);cin.tie(0);
+#define ses   "\n"
+
+string digit="0123456789";
+string small="abcdefghijklmnopqrstuvwxyz";
+
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1}; // 8-direction.......
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[]= {1,-1,0,0}; // 4-direction...........
+int dy4[]= {0,0,1,-1};
+
+const int N=1e6+5;
+const int MOD=1e9+7;
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////
+
+//main function is here.........
+int main(){
+    ll tc;
+    cin>>tc;
+    for(ll i=1;i<=tc;i++){
+        ll a,b,c,ans=0;
+        cin>>a>>b>>c;
+        if(((a*a)+(b*b))==(c*c))ans=1;
+        if(((a*a)+(c*c))==(b*b))ans=1;
+        if(((c*c)+(b*b))==(a*a))ans=1;
+        cout<<"Case "<<i<<": ";
+        ans ? cout<<"yes" : cout<<"no";
+        cout<<ses;
+    }
+}
+
+
+
+
+//Alhamdulillah
