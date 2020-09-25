f628a56162aa1653bb7094664906d125b02f105c
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2020-09-25 16:05:25 +0600

    Create C. Greg and Array.cpp

---
+++ Junior Traning sheet/DIV 2 - C1/C. Greg and Array.cpp
@@ -0,0 +1,148 @@
+///   BISMILLAHIR RAHMANIR RAHEEM
+///   ALLAH IS WATCHING ME
+///   ALLAH save us from COVID-19.Amin.
+
+///   Every soul shall taste death.
+
+///   Tag ::
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+
+///  order_of_key return number of elements less than x.
+///  find_by_order return index.
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define MOHAMMAD  ios::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define all(x)    (x).begin(), (x).end()
+#define AE        cout << fixed << setprecision(2);
+
+
+/// faster.
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1}; // 8-direction.......
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[]= {1,-1,0,0}; // 4-direction...........
+int dy4[]= {0,0,1,-1};
+
+using ll = long long;
+const ll MOD = 998244353;
+
+typedef tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+
+/// debug
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+//#else
+//#define dbg(args...)
+
+/// Modular arithmetic
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+/// Bit Manipulation
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j)) // turn off j'th bit
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+
+#define modulo(S, N) ((S) & (N - 1))   // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((int)pow(2.0, (int)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))  // turn off the last on bit
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+//***********************************************  The END **********************************************************************************************************************************
+const ll N = 1e6;
+const ll INF = 1e18;
+const ll limit = (1LL<<64) - 1;
+
+
+int main()
+{
+
+  MOHAMMAD
+
+  ll n , m , k;
+  cin >> n >> m >> k;
+
+  ll arr[n+1];
+
+  for ( ll i = 1 ; i <= n ; ++i ) cin >> arr[i];
+
+  map < ll , ll > query , op;
+  ll l , r , d;
+  ll a[m+1],b[m+1],c[m+1];
+
+  for  ( ll i = 1 ; i<=m ; ++i ){
+      cin >> l >> r >> d;
+      a[i] = l;
+      b[i] = r;
+      c[i] = d;
+  }
+
+  for  ( ll i = 1 ; i<=k ; ++i ){
+      cin >> l >> r;
+      query[l]++;
+      query[r+1]--;
+  }
+
+  for ( ll i = 1 ; i<=m ; ++i ){
+      query[i]+=query[i-1];
+  }
+
+  for( ll i = 1 ; i<=m ; ++i ){
+      ll cur = query[i];
+      c[i]*=cur;
+      op[a[i]]+=c[i];
+      op[b[i]+1]-=c[i];
+  }
+
+  for ( ll i = 1 ; i<=n ; ++i ){
+      op[i]+=op[i-1];
+  }
+
+
+  for ( ll i = 1 ; i <=n ; ++i ){
+      ll add =op[i];
+      cout << arr[i] + add << " ";
+  }
+
+
+}
+
+
+
+/*
+Explanation:
+
+
+
+----------------------------------------------------------------------------------------------------------------
+
+  Alhamdulillah
+*/
