58a352d5085ee9eb21081aca1509ae97403b6feb
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2020-10-10 11:35:48 +0600

    Create 1387 - Setu.cpp

---
+++ LightOJ/Beginners Problems/1387 - Setu.cpp
@@ -0,0 +1,63 @@
+
+///   BISMILLAHIR RAHMANIR RAHEEM
+///   ALLAH IS WATCHING ME
+///   ALLAH save us from COVID-19.Amin.
+
+///   Every soul shall taste death.
+
+///   Tag ::
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = double;
+
+const ll MAXN = 1e7+1;
+const ll INF = 1e18;
+const ll limit = (1LL<<64) - 1;
+const ld EPS   = 1e-8;
+
+
+int main()
+{
+
+
+   ll t , tc = 0;
+   cin >> t;
+
+   while ( t-- ){
+       ll m , sum = 0;
+       cin >> m;
+       cout << "Case " << ++tc << ":\n";
+       while ( m-- ){
+            string s;
+            cin >> s;
+            if ( s== "donate" ){
+               ll x;
+               cin >> x;
+               sum+=x;
+            }
+            else cout << sum << "\n";
+       }
+
+   }
+
+
+}
+
+
+
+/*
+Explanation:
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------
+
+
+
+  Alhamdulillah
+*/
