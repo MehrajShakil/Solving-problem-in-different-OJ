ef156cc29e5b6d0cf18fa6c5c6da28b675b2ce9f
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2021-05-27 20:18:32 +0600

    Update Triangle Everywhere.cpp

--- Codechef/DSA Learning Series/Easy Problems to Get Started/Triangle Everywhere.cpp
+++ Codechef/DSA Learning Series/Easy Problems to Get Started/Triangle Everywhere.cpp
@@ -84,7 +84,7 @@ void Solution ( int tc ){
 
   sort ( arr , arr + 3 );
 
-  if ( arr[0]+arr[1] < arr[2] ) cout << "-1\n";
+  if ( arr[0]+arr[1] <= arr[2] ) cout << "-1\n";
   else if ( arr[0] == arr[2] ) cout << "1\n";
   else if ( arr[0]!=arr[1] and arr[1]!=arr[2] ) cout << "3\n";
   else cout << "2\n";
