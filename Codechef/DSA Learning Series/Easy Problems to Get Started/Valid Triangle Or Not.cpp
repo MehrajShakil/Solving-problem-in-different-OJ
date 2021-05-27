9deab4b216fa8434347e59f529e0a336c26e559b
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2021-05-27 20:18:54 +0600

    Update Valid Triangle Or Not.cpp

--- Codechef/DSA Learning Series/Easy Problems to Get Started/Valid Triangle Or Not.cpp
+++ Codechef/DSA Learning Series/Easy Problems to Get Started/Valid Triangle Or Not.cpp
@@ -84,7 +84,7 @@ void Solution ( int tc ){
 
   sort ( arr , arr + 3 );
 
-  if ( arr[0]+arr[1] < arr[2] ) cout << "NO\n";
+  if ( arr[0]+arr[1] <= arr[2] ) cout << "NO\n";
   else cout << "YES\n";
 
   return;
