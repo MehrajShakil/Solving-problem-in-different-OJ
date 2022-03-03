605136375b20ec2aee47e55beb280bf72a6205b3
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2022-03-03 10:55:21 +0600

    Smallest cycle length, Solving with bfs

---
+++ CSES/Additional Problems/Graph Girth.cpp
@@ -0,0 +1,211 @@
+///   BISMILLAHIR RAHMANIR RAHEEM
+///   ALLAH IS WATCHING ME
+
+
+///   Every soul shall taste death.
+
+#include<bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+
+
+///  order_of_key return number of elements less than x.
+///  find_by_order return index.
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define MUHAMMAD        ios::sync_with_stdio(0);cin.tie(0);
+#define all(x)          (x).begin(), (x).end()
+#define AE              cout << fixed << setprecision(10);
+#define ld              double
+#define f               first
+#define ss              second
+
+
+/// faster.
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1}; // 8-direction.......
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[]= {1,-1,0,0}; // 4-direction...........
+int dy4[]= {0,0,1,-1};
+
+
+using ll = long long int;
+const ll MOD =  1e9+7;
+const ld PI = acos(-1.0);
+
+
+typedef tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef tree< int, null_type, greater<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set_boro;
+
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+
+//debug
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+//#else
+//#define dbg(args...)
+
+/// Modular arithmetic
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+/// Geometry section.
+ld degree_to_radian( ld x ){
+	ld ans = ((ld)(acos(-1.0)/180.0)*x);
+	return ans;
+}
+
+//***********************************************  The END **********************************************************************************************************************************
+
+const ll N = 2500+1;
+const ll INF = 1e18;
+const ld EPS   = 1e-9;
+const ll limit = (1LL<<32) - 1;
+const ll K = 1e5+1;
+
+/// --------------------------------------------------------------
+
+vector < int > e[N];
+vector<int>disU,disV,dis(N+1);
+
+void bfs(int node, int u){
+    dis[node] = 0;
+    queue<int> q;
+    q.push(node);
+
+    while(q.size()){
+        int node = q.front();
+        q.pop();
+
+        for(auto child:e[node]){
+            if(child==u) continue;
+            if(dis[node]+1<dis[child]){
+                dis[child] = dis[node]+1;
+                q.push(child);
+            }
+        }
+    }
+}
+
+void Solution ( int tc ){
+
+  int n,m;
+  cin >> n >> m;
+
+  int arr[m+1], brr[m+1];
+
+  for ( int i = 1 ; i<=m ; ++i ) {
+    cin >> arr[i] >> brr[i];
+    e[arr[i]].push_back(brr[i]);
+    e[brr[i]].push_back(arr[i]);
+  }
+
+  int len = n+1;
+
+
+
+  for ( int i = 1 ; i<=m ; ++i ) {
+    int u = arr[i];
+    int v = brr[i];
+
+    disU.resize(n+1,n+1);
+    disV.resize(n+1,n+1);
+
+    for(int j = 1 ; j<=n ; ++j ) dis[j] = n+1;
+    bfs(u,v);
+    for(int j = 1 ; j<=n ; ++j ) {
+        disU[j] = dis[j];
+    }
+
+
+    for(int j = 1 ; j<=n ; ++j ) dis[j] = n+1;
+    bfs(v,u);
+    for(int j = 1 ; j<=n ; ++j ) {
+        disV[j] = dis[j];
+    }
+
+    for(int j = 1 ; j<=n ; ++j ) {
+        if(disU[j]!=n+1 and disV[j]!=n+1 and j!=u and j!=v ){
+            len = min ( len , disU[j]+disV[j]+1);
+        }
+    }
+  }
+
+  if(len==n+1) len = -1;
+  cout << len << "\n";
+
+  return;
+}
+
+int main()
+{
+
+   MUHAMMAD;
+
+
+
+   /// AE;
+
+   /*
+   #ifdef OJ
+        freopen("window.in", "r", stdin);
+        freopen("output.txt", "w", stdout);
+   #endif
+   */
+
+
+    int testcase = 1 , tc = 0;
+
+
+    // cin >> testcase;
+
+    for ( int i = 1 ; i <= testcase ; ++i ){
+       Solution( ++tc );
+    }
+
+
+    return 0;
+}
+
+
+  /// __mhs, Do you check
+  /// -> overflow
+  /// -> array bound.
+
+
+/*
+
+ LCA:
+
+ https://codeforces.com/contest/1304/problem/E
+
+Explanation:
+
+ Time :
+
+
+----------------------------------------------------------------------------------------------------------------
+
+
+
+  Alhamdulillah
+*/
